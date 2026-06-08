2e2ff19eb76818011d016467249a59970be12592
Rohail Ahmad <rohailrao07@gmail.com>
2026-06-08 23:16:31 +0500

    add tokens

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 const userSchema = new Schema(
   {
@@ -48,6 +50,55 @@ const userSchema = new Schema(
   }
 );
 
+userSchema.pre("save", async function ()  {
+
+  if(!this.modifiedPaths("password")) return next()
+
+  this.password = bcrypt.hash(this.password,10)
+
+  next()
+
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+
+  return await bcrypt.compare(password, this.password)
+
+}
+
+userSchema.method.generateAccessToken = function () {
+
+  return jwt.sign({
+
+    _id:this._id,
+    email:this.email,
+    username:this.username,
+    fullname:this.fullname,
+
+  },
+
+  process.env.ACCESS_TOKEN_SECRET,
+
+  {expiresIn:process.env.ACCESS_TOKEN_EXPIRY}
+  
+  );
+}
+
+
+userSchema.method.generateRefreshToken = function () {
+
+  return jwt.sign({
+
+    _id:this._id,
+
+  },
+
+  process.env.REFRESH_TOKEN_SECRET,
+
+  {expiresIn:process.env.REFRESH_TOKEN_EXPIRY}
+
+  );
+}
 const User = mongoose.model("User", userSchema);
 
 export default User;
